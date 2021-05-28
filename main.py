0901d27b36bf75b5d6376465a139a080cdc90c4e
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-28 14:58:38 +0200

    Final udpate

--- main.py
+++ main.py
@@ -1,14 +1,25 @@
 import smtplib
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 
 # please note go to https://myaccount.google.com/lesssecureapps and
 # turn it off to allow gmail to send
 # creates SMTP session
 
-s = smtplib.SMTP("smtp.gmail.com", 587)
 sender_email_id = "brentleejohnson73@gmail.com"
-receiver_email_id = ["naeemahndavis@gmail.com", "jasoncee017@gmail.com", "jaydenmay040@gmail.com"]
+receiver_email_id = ["naeemahndavis@gmail.com", "jasoncee017@gmail.com", "thapelo@lifechoices.co.za"]
 password = input("Password: ")
+Subject = "Testrun"
+msg = MIMEMultipart()
+msg['From'] = sender_email_id
+msg['To'] = ", ".join(receiver_email_id)
+msg['Subject'] = Subject
+body = "Hey\n"
+body = body + "Testing my coding for an email"
+msg.attach(MIMEText(body, 'plain'))
+text = msg.as_string()
+s = smtplib.SMTP('smtp.gmail.com', 587)
 
 # start TLS for security
 s.starttls()
@@ -17,9 +28,9 @@ s.starttls()
 s.login(sender_email_id, password)
 
 # sending the mail
-message = "Greetings\n"
-message = message + "Testing email coding"
-s.sendmail(sender_email_id, receiver_email_id, message)
+s.sendmail(sender_email_id, receiver_email_id, text)
+
+# s.sendmail(sender_email_id, receiver_email_id, text)
 
 # terminating the session
 s.quit()

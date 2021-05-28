90f76254ac99adf4067584c48226e8327d6a52a2
Brent Lee Johnson <brentleejohnson73@gmail.com>
2021-05-28 14:04:36 +0200

    Update

--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import smtplib
+
+
 # please note go to https://myaccount.google.com/lesssecureapps and
 # turn it off to allow gmail to send
 # creates SMTP session
+
 s = smtplib.SMTP("smtp.gmail.com", 587)
 sender_email_id = "brentleejohnson73@gmail.com"
-receiver_email_id = "naeemahndavis@gmail.com"
-password = input("Enter senders email password: ")
+receiver_email_id = ["naeemahndavis@gmail.com", "jasoncee017@gmail.com", "jaydenmay040@gmail.com"]
+password = input("Password: ")
+
 # start TLS for security
 s.starttls()
+
 # authentication
 s.login(sender_email_id, password)
+
 # sending the mail
 message = "Greetings\n"
 message = message + "Testing email coding"
 s.sendmail(sender_email_id, receiver_email_id, message)
+
 # terminating the session
 s.quit()
